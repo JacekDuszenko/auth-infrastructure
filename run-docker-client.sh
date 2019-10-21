d25f9f726aef19696efb22e01668f3f8415bd4fc
Kamil <kamij98@gmail.com>
2019-10-21 07:07:54 +0000

    Resolve "Client backend"

--- run-docker-client.sh
+++ run-docker-client.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 wget https://github.com/debuerreotype/docker-debian-artifacts/blob/74e1a3304401c2eb9c6624ae1056d0a438c15189/jessie/rootfs.tar.xz
-sudo docker build . -t docker-client
-sudo docker run docker-client
+sudo docker build . -t docker-client_backend
+sudo docker run docker-client_backend
