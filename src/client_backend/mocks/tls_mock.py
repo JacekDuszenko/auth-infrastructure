c98b7669cfcc256c96a9d55b9c631e3e7d8c0725
Kamil <kamij98@gmail.com>
2019-10-21 07:07:54 +0000

    Resolve "Client backend"

---
+++ src/client_backend/mocks/tls_mock.py
@@ -0,0 +1,26 @@
+from unittest.mock import MagicMock
+
+# remove when server will be up
+SERV_TEMP_RESPONSE = b'{"authorisation": "True"}'
+
+class ssl:
+    """mock class for better encapsulation"""
+    PROTOCOL_TLS_CLIENT = 0xDEADDEAD
+    PROTOCOL_TLS_SERVER = 0xDEADFEED
+
+    @staticmethod
+    def SSLContext(foo):
+        return MagicMock()
+
+
+class socket:
+    """mock class for better encapsulation"""
+    AF_INET = 0xDEADDEAD
+    SOCK_STREAM = 0xDEADFEED
+
+    @staticmethod
+    def socket(foo, bar):
+        return MagicMock()
+
+    class timeout(Exception):
+        pass
