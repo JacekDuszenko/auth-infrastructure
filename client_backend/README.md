5869c8c86e9eb58b27f8cae46260cb773d9ed4de
Jacek Duszenko <jacek.duszenko@huuugegames.com>
2019-10-21 20:14:20 +0200

    restructured project again and fixed dockerfile

---
+++ client_backend/README.md
@@ -0,0 +1,4 @@
+# How to run
+
+## To run standalone docker image issue 
+*./run-client-backend.sh*
--- run-docker-client.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-wget https://github.com/debuerreotype/docker-debian-artifacts/blob/74e1a3304401c2eb9c6624ae1056d0a438c15189/jessie/rootfs.tar.xz
-sudo docker build . -t docker-client_backend
-sudo docker run docker-client_backend
