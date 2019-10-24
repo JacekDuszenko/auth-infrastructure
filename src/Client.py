1fdc076a761d8765575c435efc6cef3010acdc49
dawidwyrembak <dawidwyrembak@gmail.com>
2019-10-24 16:47:20 +0200

    Compile with 'python3 ./name_of_file'

--- src/Client.py
+++ src/Client.py
@@ -9,6 +9,8 @@ SERVER_HOSTNAME = '127.0.0.1'
 # context = ssl.SSLContext(ssl.PROTOCOL_TLS_CLIENT)
 context = ssl._create_unverified_context()
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM, 0) as sock:
-    with context.wrap_socket(sock, server_hostname=SERVER_HOSTNAME) as sslSock:
-        sslSock.connect((SERVER_HOSTNAME, PORT_NUMBER))
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+sslSock = context.wrap_socket(sock, server_side=False, server_hostname=SERVER_HOSTNAME)
+sslSock.connect((SERVER_HOSTNAME, PORT_NUMBER))
+print("Client started")
+sslSock.close()
