e7c86d6182f7e47d6934b183c0e218380c2895d3
Marcin Umiński <marcinuminski1998@gmail.com>
2019-11-04 16:45:24 +0100

    Add finished script for integ-test application, add network to docker-compose

--- run-integ-test.sh
+++ run-integ-test.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+NETWORK='teamprogramming2k19_integ-test'
+INTEG_TEST_APP='integ-test-app'
 
 if [ -z "${GITLAB_USERNAME}" ]; then
     echo "set GITLAB_USERNAME environment variable"
@@ -7,14 +9,13 @@ fi
 echo "Enter your gitlab password"
 docker login registry.gitlab.com --username $GITLAB_USERNAME
 
-docker-compose up -d
+docker image build -t ${INTEG_TEST_APP} integ-test/ #buduje obraz aplikacji integ-test
 
-cd integ-test
-docker image build -t integ-test .
-docker run -d integ-test
-cd ../
+docker-compose up -d  #uruchamia system kontenerów
 
-docker rm $(docker stop $(docker ps -a -q --filter ancestor=integ-test --format="{{.ID}}"))
-docker-compose down
+docker run -d ${INTEG_TEST_APP} #uruchamia kontener integ-test
+INTEG_TEST_CONTAINER=$(docker ps -a -q --filter ancestor=${INTEG_TEST_APP} --format="{{.ID}}")  #Znajduje ID kontenera na podstawie nazwy image'a
+docker network connect "${NETWORK}" "${INTEG_TEST_CONTAINER}"  #Dołącza kontener z aplikacją integ-test do networka, w którym są pozostałe aplikacje
 
-
+docker rm $(docker stop ${INTEG_TEST_CONTAINER})  #usuwa kontener integ-test
+docker-compose down #usuwa wszystkie kontenery aplikacji
